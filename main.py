e48b4ba064676438b85b71dcfa6cd54a38f1e72b
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-08-11 15:51:22 +0000

    Leap year program added
    added a program to tell if the year informed is a leap year or not.

--- main.py
+++ main.py
@@ -30,21 +30,37 @@
 ##    print("This is an odd number.")
 
 # 🚨 Don't change the code below 👇
-height = float(input("enter your height in m: "))
-weight = float(input("enter your weight in kg: "))
+#height = float(input("enter your height in m: "))
+#weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = round(weight / (height * height))
+#bmi = round(weight / (height * height))
 #bmi = int(bmi)
 
-if bmi < 18.5:
-    print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi > 18.5 and bmi < 25:
-    print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi > 25 and bmi < 30:
-    print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif bmi > 30 and bmi < 35:
-    print(f"Your BMI is {bmi}, you are obese.")
+#if bmi < 18.5:
+#    print(f"Your BMI is {bmi}, you are underweight.")
+#elif bmi > 18.5 and bmi < 25:
+#    print(f"Your BMI is {bmi}, you have a normal weight.")
+#elif bmi > 25 and bmi < 30:
+#    print(f"Your BMI is {bmi}, you are slightly overweight.")
+#elif bmi > 30 and bmi < 35:
+#    print(f"Your BMI is {bmi}, you are obese.")
+#else:
+#    print(f"Your BMI is {bmi}, you are Tais Carla.")
+
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
 else:
-    print(f"Your BMI is {bmi}, you are Tais Carla.")
+    print("Not leap year.")
