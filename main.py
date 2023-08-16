d5c6da64b43db63ebc21d7254bc0c9a315d96fc9
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-08-16 16:52:37 +0000

    Start of the treasure island game

--- main.py
+++ main.py
@@ -101,46 +101,102 @@
 #print(f"Your final bill is: ${bill}.")
 
 # 🚨 Don't change the code below 👇
-print("Welcome to the Love Calculator!")
-name1 = input("What is your name? \n")
-name2 = input("What is their name? \n")
+#print("Welcome to the Love Calculator!")
+#name1 = input("What is your name? \n")
+#name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
 
 #to begin i gotta combine the names so it's easier to count later on
-names = name1 + name2
+#names = name1 + name2
 
 #now i gotta turn the name to lower case so i can start counting
-names = names.lower()
+#names = names.lower()
 
 #time to start counting the letters and compare it to TRUE
-t = names.count("t")
-r = names.count("r")
-u = names.count("u")
-e = names.count("e")
+#t = names.count("t")
+#r = names.count("r")
+#u = names.count("u")
+#e = names.count("e")
 
 #here we gather the full quantity
-true = t + r + u + e
+#true = t + r + u + e
 
 #time to start counting the letters and compare it to LOVE
-l = names.count("l")
-o = names.count("o")
-v = names.count("v")
-e = names.count("e")
+#l = names.count("l")
+#o = names.count("o")
+#v = names.count("v")
+#e = names.count("e")
 
 #here we gather the full quantity
-love = l + o + v + e
+#love = l + o + v + e
 
 #combining total score
-score = int(str(true) + str(love))
+#score = int(str(true) + str(love))
 #gotta convert SCORE to INT
 #finalscore = int(score)
 
 #time to print the results
-if (score < 10) or (score > 90):
-    print(f"Your score is {score}, you go together like coke and mentos.")
-elif (score >= 40) and (score <= 50):
-    print(f"Your score is {score}, you are alright together.")
+#if (score < 10) or (score > 90):
+#    print(f"Your score is {score}, you go together like coke and mentos.")
+#elif (score >= 40) and (score <= 50):
+#    print(f"Your score is {score}, you are alright together.")
+#else:
+#    print(f"Your score is {score}.")
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[Alaaaan]
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+option1 = str(input("Okay, adventurer! You're at a crossroad with a map and you have two options, to turn left or right. The instruction on the map is not clear enough and you have to make a choice. To turn RIGHT or LEFT. What will you do? \n"))
+#loweroption1 = option1.lower()
+if option1 == 'right':
+  print("You may go ahead, adventurer. Try your luck!")
+  option2 = str(input("As you go walking down the street you chose, you find a broken bridge. The river seems quiet and unharmful. You have to cross this river so you may get closer to the treasure. According to the map you had three options: to cross using the BRIDGE, SWIM to the other side or take the BOAT. What will you do? \n"))
+  if option2 == 'boat':
+    print("You paddle to the other side safely")
+    option3 = str(input('''As you arrive at the other side the map tells you to go straight on the road you find.
+    At the end of the road you find a suspicious house, as described in the map. You see three doors: a BLACK door, a WHITE door and BROWN door.
+    According to the map two of these doors have deadly traps behind them and it's not clear which doors have traps.
+    Which door will you choose? \n'''))
+    if option3 == "black":
+      print("Congratulation, adventurer. You found the treasure and a safe haven.")
+    elif option3 == "white":
+      print("Attatched to the door is an explosive and you explode to pieces. Game over! Start again!")
+    elif option3 == "brown":
+      print("Behind this door you unleash an army of zombies and get eaten to death. Game over! Start again!")
+    else:
+      print("By doing nothing you realize that the air is poisonous and choke to death. Game over! Start again!")
+  elif option2 == 'swim':
+    print("You're attacked by piranas and die. Game over! Start again!")
+  else:
+    print("You stay on the beach, rest and a group of cannibals eat you. Game over! Start again!")
+  
 else:
-    print(f"Your score is {score}.")
+  print('Game over. Start Again!')
+  
